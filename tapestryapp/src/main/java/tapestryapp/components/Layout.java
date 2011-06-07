0120955d69a98579ea8f818c4f4da3cbd345ceb6
kaosko <kaosko@apache.org>
2011-06-07 08:13:06 -0700

    Remove unused source

--- tapestryapp/src/main/java/tapestryapp/components/Layout.java
+++ tapestryapp/src/main/java/tapestryapp/components/Layout.java
@@ -2,10 +2,8 @@ package tapestryapp.components;
 
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.Block;
-import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
 
 /**
  * Layout component for pages of application tapestryapp.
@@ -18,28 +16,10 @@ public class Layout
     private String title;
 
     @Property
-    private String pageName;
-
-    @Property
     @Parameter(defaultPrefix = BindingConstants.LITERAL)
     private String sidebarTitle;
 
     @Property
     @Parameter(defaultPrefix = BindingConstants.LITERAL)
     private Block sidebar;
-
-    @Inject
-    private ComponentResources resources;
-
-    public String getClassForPageName()
-    {
-      return resources.getPageName().equalsIgnoreCase(pageName)
-             ? "current_page_item"
-             : null;
-    }
-
-    public String[] getPageNames()
-    {
-      return new String[] { "Index", "About", "Contact" };
-    }
 }
